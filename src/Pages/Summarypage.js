7713e5d62880406025e23bff23631d20e28c8da7
Aysha Muhammed <m.aishat9890@gmail.com>
2022-08-02 05:14:19 -0700

    update

--- src/Pages/Summarypage.js
+++ src/Pages/Summarypage.js
@@ -11,33 +11,35 @@ const Summarypage = () => {
                 <img src={illustration} alt=''/>
             </div>
         </header>
-        
-        <div className='summary'>
-            <h1>Order Summary</h1>
-            <p>You can now listen to millions of songs, audiobooks, and
-                podcast on any device aanywhere you like
-            </p>
-        </div>
+        <body>
+                
+                <div className='summary'>
+                    <h1>Order Summary</h1>
+                    <p>You can now listen to millions of songs, audiobooks, and
+                        podcast on any device aanywhere you like
+                    </p>
+                </div>
 
-        <div className='plan'>
-            <div className='icon'>
-                <img src={music} alt=''/>
-            </div>
-            
-            <div className='price'>
-                <h2>Annual Plan</h2>
-                <h3>$59.99 dollar per year</h3>
-            </div>
-            <div className='change'>
-                <h4>Change</h4>
-            </div>
-        </div>
-        <div className='proceed'>
-            <button className='proceedpayment'>Proceed to payment</button>
-        </div>
-        <div className='proceed'>
-            <button className='cancelPayment'>Cancel Order</button>
-        </div>
+                <div className='plan'>
+                    <div className='icon'>
+                        <img src={music} alt=''/>
+                    </div>
+                    
+                    <div className='price'>
+                        <h2>Annual Plan</h2>
+                        <h3>$59.99 dollar per year</h3>
+                    </div>
+                    <div className='change'>
+                        <h4>Change</h4>
+                    </div>
+                </div>
+                <div className='proceed'>
+                    <button className='proceedpayment'>Proceed to payment</button>
+                </div>
+                <div className='proceed'>
+                    <button className='cancelPayment'>Cancel Order</button>
+                </div>
+        </body>
     </div>
 
   )
